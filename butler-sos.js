490fd27521c7c9c98584ad6ca51f9693a351ad4e
mountaindude <mountaindude@ptarmiganlabs.com>
2017-04-21 21:12:33 +0200

    Fixed merge conflicts

--- butler-sos.js
+++ butler-sos.js
@@ -17,7 +17,6 @@ globals.logger.info('Starting Butler SOS');
 
 
 
-
 function postToInfluxdb(host, serverName, body) {
 
     // Calculate server uptime
@@ -149,7 +148,7 @@ function postToInfluxdb(host, serverName, body) {
 
 function postToMQTT(host, serverName, body) {
 
-    // Get base MQT topic
+    // Get base MQTT topic
     var baseTopic = globals.config.get('Butler-SOS.mqttConfig.baseTopic');
 
     // Send to MQTT
@@ -200,7 +199,6 @@ function getStatsFromSense(host, serverName) {
             globals.logger.verbose('Received ok response from ' + serverName);
             globals.logger.debug(body);
 
-<<<<<<< HEAD
             // Post to MQTT (if enabled)
             if ( globals.config.get('Butler-SOS.mqttConfig.enableMQTT') ) {
                 globals.logger.debug('Calling MQTT posting method');
@@ -212,36 +210,6 @@ function getStatsFromSense(host, serverName) {
                 globals.logger.debug('Calling Influxdb posting method');
                 postToInfluxdb(host, serverName, body);
             }
-=======
-            // Get base MQT topic
-            var baseTopic = globals.config.get('Butler-SOS.mqttConfig.baseTopic');
-
-            // Send to MQTT
-            globals.mqttClient.publish(baseTopic + serverName + '/version', body.version);
-            globals.mqttClient.publish(baseTopic + serverName + '/started', body.started);
-            globals.mqttClient.publish(baseTopic + serverName + '/mem/comitted', body.mem.comitted.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/mem/allocated', body.mem.allocated.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/mem/free', body.mem.free.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cpu/total', body.cpu.total.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/session/active', body.session.active.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/session/total', body.session.total.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/active_docs', body.apps.active_docs.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/loaded_docs', body.apps.loaded_docs.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/calls', body.apps.calls.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/selections', body.apps.selections.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/users/active', body.users.active.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/users/total', body.users.total.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/hits', body.cache.hits.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/lookups', body.cache.lookups.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/added', body.cache.added.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/replaced', body.cache.replaced.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/bytes_added', body.cache.bytes_added.toString());
-
-            if (body.cache.lookups > 0) {
-                globals.mqttClient.publish(baseTopic + serverName + '/cache/hit_ratio', Math.floor(body.cache.hits / body.cache.lookups * 100).toString());
-                console.log(Math.floor(body.cache.hits / body.cache.lookups * 100).toString());
-            } 
->>>>>>> origin/master
         }
     })
 }
