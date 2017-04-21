68657cfd3b6f1ef9eba2786d14c68b0c6a391472
mountaindude <mountaindude@ptarmiganlabs.com>
2017-04-21 20:57:23 +0200

    v1.0 check-in
    Adding storage to Influxdb
    Independently enable/disable data to MQTT and Influxdb
    New logo
    Sample Grafana dashboard config file

--- butler-sos.js
+++ butler-sos.js
@@ -17,18 +17,182 @@ globals.logger.info('Starting Butler SOS');
 
 
 
-function postStatsToMQTT(host, serverName) {
+
+function postToInfluxdb(host, serverName, body) {
+
+    // Calculate server uptime
+
+    var dateTime = Date.now();
+    var timestamp = Math.floor(dateTime);
+
+    var str = body.started;
+    var year = str.substring(0, 4);
+    var month = str.substring(4, 6);
+    var day = str.substring(6, 8);
+    var hour = str.substring(9, 11);
+    var minute = str.substring(11, 13);
+    var second = str.substring(13, 15);
+    var dateTimeStarted = new Date(year, month - 1, day, hour, minute, second);
+    var timestampStarted = Math.floor(dateTimeStarted);
+
+    var diff = timestamp - timestampStarted;
+
+
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(diff);
+
+    var days = Math.trunc((diff) / (1000 * 60 * 60 * 24))
+
+    // Hours part from the timestamp
+    var hours = date.getHours();
+
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime = days + ' days, ' + hours + 'h ' + minutes.substr(-2) + 'm ' + seconds.substr(-2) + 's';
+
+
+    // Write the whole reading to Influxdb
+    globals.influx.writePoints([{
+                measurement: 'sense_server',
+                tags: {
+                    host: serverName
+                },
+                fields: {
+                    version: body.version,
+                    started: body.started,
+                    uptime: formattedTime
+                }
+            },
+            {
+                measurement: 'mem',
+                tags: {
+                    host: serverName
+                },
+                fields: {
+                    comitted: body.mem.comitted,
+                    allocated: body.mem.allocated,
+                    free: body.mem.free
+                }
+            },
+            {
+                measurement: 'apps',
+                tags: {
+                    host: serverName
+                },
+                fields: {
+                    active_docs_count: body.apps.active_docs.length,
+                    loaded_docs_count: body.apps.loaded_docs.length,
+                    calls: body.apps.calls,
+                    selections: body.apps.selections
+                }
+            },
+            {
+                measurement: 'cpu',
+                tags: {
+                    host: serverName
+                },
+                fields: {
+                    total: body.cpu.total
+                }
+            },
+            {
+                measurement: 'session',
+                tags: {
+                    host: serverName
+                },
+                fields: {
+                    active: body.session.active,
+                    total: body.session.total
+                }
+            },
+            {
+                measurement: 'users',
+                tags: {
+                    host: serverName
+                },
+                fields: {
+                    active: body.users.active,
+                    total: body.users.total
+                }
+            },
+            {
+                measurement: 'cache',
+                tags: {
+                    host: serverName
+                },
+                fields: {
+                    hits: body.cache.hits,
+                    lookups: body.cache.lookups,
+                    added: body.cache.added,
+                    replaced: body.cache.replaced,
+                    bytes_added: body.cache.bytes_added
+                }
+            }
+
+        ])
+        .then(err => {
+            globals.logger.verbose('Sent data to Influxdb');
+        })
+
+        .catch(err => {
+            console.error(`Error saving data to InfluxDB! ${err.stack}`)
+        })
+
+}
+
+
+function postToMQTT(host, serverName, body) {
+
+    // Get base MQT topic
+    var baseTopic = globals.config.get('Butler-SOS.mqttConfig.baseTopic');
+
+    // Send to MQTT
+    globals.mqttClient.publish(baseTopic + serverName + '/version', body.version);
+    globals.mqttClient.publish(baseTopic + serverName + '/started', body.started);
+    globals.mqttClient.publish(baseTopic + serverName + '/mem/comitted', body.mem.comitted.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/mem/allocated', body.mem.allocated.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/mem/free', body.mem.free.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/cpu/total', body.cpu.total.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/session/active', body.session.active.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/session/total', body.session.total.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/apps/active_docs', body.apps.active_docs.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/apps/loaded_docs', body.apps.loaded_docs.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/apps/calls', body.apps.calls.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/apps/selections', body.apps.selections.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/users/active', body.users.active.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/users/total', body.users.total.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/cache/hits', body.cache.hits.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/cache/lookups', body.cache.lookups.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/cache/added', body.cache.added.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/cache/replaced', body.cache.replaced.toString());
+    globals.mqttClient.publish(baseTopic + serverName + '/cache/bytes_added', body.cache.bytes_added.toString());
+
+    if (body.cache.lookups > 0) {
+        globals.mqttClient.publish(baseTopic + serverName + '/cache/hit_ratio', Math.floor(body.cache.hits / body.cache.lookups * 100).toString());
+    }
+}
+
+
+
+
+function getStatsFromSense(host, serverName) {
     request({
         followAllRedirects: true,
         url: 'https://' + host + '/healthcheck/engine/healthcheck/',
         headers: {
-            'Cache-Control':'no-cache'
+            'Cache-Control': 'no-cache'
         },
         json: true
     }, function (error, response, body) {
 
         // Check for error
-        if(error){
+        if (error) {
             return globals.logger.error('Error:', error);
         }
 
@@ -36,43 +200,32 @@ function postStatsToMQTT(host, serverName) {
             globals.logger.verbose('Received ok response from ' + serverName);
             globals.logger.debug(body);
 
-            // Get base MQT topic
-            var baseTopic = globals.config.get('Butler-SOS.mqttConfig.baseTopic');
+            // Post to MQTT (if enabled)
+            if ( globals.config.get('Butler-SOS.mqttConfig.enableMQTT') ) {
+                globals.logger.debug('Calling MQTT posting method');
+                postToMQTT(host, serverName, body);
+            }
 
-            // Send to MQTT
-            globals.mqttClient.publish(baseTopic + serverName + '/version', body.version);
-            globals.mqttClient.publish(baseTopic + serverName + '/started', body.started);
-            globals.mqttClient.publish(baseTopic + serverName + '/mem/comitted', body.mem.comitted.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/mem/allocated', body.mem.allocated.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/mem/free', body.mem.free.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cpu/total', body.cpu.total.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/session/active', body.session.active.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/session/total', body.session.total.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/active_docs', body.apps.active_docs.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/loaded_docs', body.apps.loaded_docs.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/calls', body.apps.calls.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/apps/selections', body.apps.selections.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/users/active', body.users.active.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/users/total', body.users.total.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/hits', body.cache.hits.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/lookups', body.cache.lookups.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/added', body.cache.added.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/replaced', body.cache.replaced.toString());
-            globals.mqttClient.publish(baseTopic + serverName + '/cache/bytes_added', body.cache.bytes_added.toString());
+            // Post to Influxdb (if enabled)
+            if ( globals.config.get('Butler-SOS.influxdbConfig.enableInfluxdb') ) {
+                globals.logger.debug('Calling Influxdb posting method');
+                postToInfluxdb(host, serverName, body);
+            }
         }
-        
     })
 }
 
 
-setInterval(function() { 
+
+setInterval(function () {
     globals.logger.verbose('Event started: Statistics collection');
 
     var serverList = globals.config.get('Butler-SOS.serversToMonitor.servers');
-    serverList.forEach(function(server) {
+    serverList.forEach(function (server) {
         globals.logger.verbose('Getting stats for server: ' + server.serverName);
-        postStatsToMQTT(server.host, server.serverName); 
+
+
+        getStatsFromSense(server.host, server.serverName);
     });
 
 }, globals.config.get('Butler-SOS.pollingInterval'));
-
--- globals.js
+++ globals.js
@@ -1,13 +1,121 @@
 var mqtt = require('mqtt');
 var config = require('config');
 var winston = require('winston');
+const Influx = require('influx');
+
+
 
 // Set up logger with timestamps and colors
-var logger = new (winston.Logger)({
+var logger = new(winston.Logger)({
     transports: [
-      new (winston.transports.Console)({'timestamp':true, 'colorize':true})
+        new(winston.transports.Console)({
+            'timestamp': true,
+            'colorize': true
+        })
     ]
 });
+
+
+// Set up Influxdb client
+const influx = new Influx.InfluxDB({
+    host: config.get('Butler-SOS.influxdbConfig.hostIP'),
+    database: config.get('Butler-SOS.influxdbConfig.dbName'),
+    schema: [
+        {
+            measurement: 'sense_server',
+            fields: {
+                version: Influx.FieldType.STRING,
+                started: Influx.FieldType.STRING,
+                uptime: Influx.FieldType.STRING
+            },
+            tags: [
+                'host'
+            ]
+        },
+        {
+            measurement: 'mem',
+            fields: {
+                comitted: Influx.FieldType.INTEGER,
+                allocated: Influx.FieldType.INTEGER,
+                free: Influx.FieldType.INTEGER
+            },
+            tags: [
+                'host'
+            ]
+        },
+        {
+            measurement: 'apps',
+            fields: {
+                active_docs_count: Influx.FieldType.INTEGER,
+                loaded_docs_count: Influx.FieldType.INTEGER,
+                calls: Influx.FieldType.INTEGER,
+                selections: Influx.FieldType.INTEGER
+            },
+            tags: [
+                'host'
+            ]
+        },
+        {
+            measurement: 'cpu',
+            fields: {
+                total: Influx.FieldType.INTEGER
+            },
+            tags: [
+                'host'
+            ]
+        },
+        {
+            measurement: 'session',
+            fields: {
+                active: Influx.FieldType.INTEGER,
+                total: Influx.FieldType.INTEGER
+            },
+            tags: [
+                'host'
+            ]
+        },
+        {
+            measurement: 'users',
+            fields: {
+                active: Influx.FieldType.INTEGER,
+                total: Influx.FieldType.INTEGER
+            },
+            tags: [
+                'host'
+            ]
+        },
+        {
+            measurement: 'cache',
+            fields: {
+                hits: Influx.FieldType.INTEGER,
+                lookups: Influx.FieldType.INTEGER,
+                added: Influx.FieldType.INTEGER,
+                replaced: Influx.FieldType.INTEGER,
+                bytes_added: Influx.FieldType.INTEGER
+            },
+            tags: [
+                'host'
+            ]
+        }
+    ]
+})
+
+
+influx.getDatabaseNames()
+    .then(names => {
+        if (!names.includes(config.get('Butler-SOS.influxdbConfig.dbName'))) {
+            logger.info('Creating Influx database.');
+            return influx.createDatabase(config.get('Butler-SOS.influxdbConfig.dbName'));
+        }
+    })
+    .then(() => {
+        logger.info('Connected to Influx database.');
+        return;
+    })
+    .catch(err => {
+        logger.error(`Error creating Influx database!`);
+    })
+
 
 
 // ------------------------------------
@@ -23,7 +131,8 @@ var mqttClient  = mqtt.connect('mqtt://<IP of MQTT server>', {
 
 
 module.exports = {
-  config,
-  mqttClient,
-  logger
+    config,
+    mqttClient,
+    logger,
+    influx
 };
